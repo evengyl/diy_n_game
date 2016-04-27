f03d19d8b6ecf30fdc5e35f715e898fe6ff281cd
evengyl <dark.evengyl@gmail.com>
2016-04-27 17:01:39 +0200

    travail sur les images et les effet flatdesign

--- vues/game_home.php
+++ vues/game_home.php
@@ -1,105 +1,19 @@
 <div class="col-xs-12 col-without-padding col-without-radius content_game">
 	__TPL_nav__
 
-
-	
-
 	<div class="col-xs-12 col-md-9">
-
-		<div class="collapse col-lg-12" id="culture_vg">
-			<div class="thumbnail col-xs-12" style="background:#232D3B; margin-top:10px;">
-				<h2 style="font-size:24px; margin-top:0px; color:white;" class="col-xs-12">Culture VG</h2>
-				<div class="col-xs-8" style="height:100%;">
-					<center><img style="max-height:150px;" src="../public/images/plantes.png" alt="..." class="img-circle img-responsive"></center>
-				</div>
-
-				<div class="col-xs-4" style="margin-bottom:10px;">
-					<ul class="list-group">
-						<li class="col-xs-12 list-group-item pull-right"><b>Level : </b> 70</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Production /h : </b> 425692</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Plantes disponibles : </b>156548941</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Littres de VG disponibles : </b>1565</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Prix next level : </b>1526301</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Temps de construction : </b>14h 15m</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
-
-		<div class="collapse col-lg-12" id="usines_pg">
-			<div class="thumbnail col-xs-12" style="background:#232D3B; margin-top:10px;">
-				<h2 style="font-size:24px; margin-top:0px; color:white;" class="col-xs-12">Usines PG</h2>
-				<div class="col-xs-8" style="height:100%;">
-					<center><img style="max-height:150px;" src="../public/images/plantes.png" alt="..." class="img-circle img-responsive"></center>
-				</div>
-				<div class="col-xs-4" style="margin-bottom:10px;">
-					<ul class="list-group">
-						<li class="col-xs-12 list-group-item pull-right"><b>Level : </b> 70</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Production /h : </b> 425692</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Glycérole disponibles : </b>156548941</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Littres de PG disponibles : </b>1565</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Prix next level : </b>1526301</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Temps de construction : </b>14h 15m</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
-
-		<div class="collapse col-lg-12" id="labos_base">
-			<div class="thumbnail col-xs-12" style="background:#232D3B; margin-top:10px;">
-				<h2 style="font-size:24px; margin-top:0px; color:white;" class="col-xs-12">Labos des créations des bases</h2>
-				<div class="col-xs-8" style="height:100%;">
-					<center><img style="max-height:150px;" src="../public/images/plantes.png" alt="..." class="img-circle img-responsive"></center>
-				</div>
-				<div class="col-xs-4" style="margin-bottom:10px;">
-					<ul class="list-group">
-						<li class="col-xs-12 list-group-item pull-right"><b>Level : </b> 70</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Réduction des coups en % : </b> 80</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Prix next level : </b>1526301</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Temps de construction : </b>14h 15m</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
-		<div class="collapse col-lg-12" id="melange_base">
-			<div class="thumbnail col-xs-12" style="background:#232D3B; margin-top:10px;">
-				<h2 style="font-size:24px; margin-top:0px; color:white;" class="col-xs-12">Mélangeur de vos bases en littres de bases prètes à l'emploi</h2>
-				<div class="col-xs-8" style="height:100%;">
-					<center><img style="max-height:150px;" src="../public/images/plantes.png" alt="..." class="img-circle img-responsive"></center>
-				</div>
-				<div class="col-xs-4" style="margin-bottom:10px;">
-					<ul class="list-group">
-						<li class="col-xs-12 list-group-item pull-right"><b>Level : </b> 70</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Réduction des coups en % : </b> 80</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Littres de 50PG/50VG : </b>150</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Littres de 20PG/80VG : </b>150</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Littres de 80PG/20VG : </b>150</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Littres de 100VG : </b>150</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Prix next level : </b>1526301</li>
-						<li class="col-xs-12 list-group-item pull-right"><b>Temps de construction : </b>14h 15m</li>
-
-					</ul>
-				</div>
-			</div>
-		</div>
-
-
-
 		<div class="col-lg-12" style="margin-bottom:15px;">
 			<div class="col-lg-12" style="background:#232D3B;">
 				<h1 class='col-xs-12 title' style="margin-bottom:15px; ">
 					Listes des batiments de production, pour une vues plus précises, passer par le menu de gauche
 				</h1>
 
-				<ul class="col-md-6 col-lg-6 grid cs-style-4">
+				<ul class="col-md-6 col-lg-4 grid cs-style-4">
 					<center><li>
 						<figure>
 							<div><img src="images/plante_icon.png" alt="img04"></div>
 							<figcaption>
-								<h3>Culture VG</h3>
+								<h3 style="font-size:20px;">Culture VG</h3>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Level : </b><br>70</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Production /h : </b><br>425692</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Prix next level : </b><br>1526301</li>
@@ -109,12 +23,12 @@
 					</li></center>
 				</ul>
 
-				<ul class="col-md-6 col-lg-6 grid cs-style-4">
+				<ul class="col-md-6 col-lg-4 grid cs-style-4">
 					<center><li>
 						<figure>
 							<div><img src="images/plante_icon.png" alt="img04"></div>
 							<figcaption>
-								<h3>Settings</h3>
+								<h3 style="font-size:20px;">Settings</h3>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Level : </b><br>70</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Production /h : </b><br>425692</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Prix next level : </b><br>1526301</li>
@@ -124,12 +38,12 @@
 					</li></center>
 				</ul>
 
-				<ul class="col-md-6 col-lg-6 grid cs-style-4">
+				<ul class="col-md-6 col-lg-4 grid cs-style-4">
 					<center><li>
 						<figure>
 							<div><img src="images/plante_icon.png" alt="img04"></div>
 							<figcaption>
-								<h3>Settings</h3>
+								<h3 style="font-size:20px;">Settings</h3>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Level : </b><br>70</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Production /h : </b><br>425692</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Prix next level : </b><br>1526301</li>
@@ -139,12 +53,12 @@
 					</li></center>
 				</ul>
 
-				<ul class="col-md-6 col-lg-6 grid cs-style-4">
+				<ul class="col-md-6 col-lg-4 grid cs-style-4">
 					<center><li>
 						<figure>
 							<div><img src="images/plante_icon.png" alt="img04"></div>
 							<figcaption>
-								<h3>Settings</h3>
+								<h3 style="font-size:20px;">Settings</h3>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Level : </b><br>70</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Production /h : </b><br>425692</li>
 								<li style="font-size:12px;" class="col-xs-12 pull-right"><b>Prix next level : </b><br>1526301</li>
--- vues/top_head.php
+++ vues/top_head.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="content-type" content="text/html" charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="author" content="Baudoux Loïc" />
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300' rel='stylesheet' type='text/css'>
 	<script type="text/javascript" type="../public/js/less-1.7.4.min.js"></script>
 	<script type="text/javascript" src="../public/js/jquery-2.1.3.min.js"></script>
 	<script type="text/javascript" src="../public/js/animo.js"></script>	
