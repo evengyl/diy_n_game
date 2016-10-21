65a33d8d660c6378a9aa68c0a1fb708b3223d027
evengyl <dark.evengyl@gmail.com>
2016-10-21 14:39:19 +0200

    ajout de la base et amélioration footer

--- vues/footer.php
+++ vues/footer.php
@@ -1,3 +1,11 @@
-<div class="col-lg-12 footer">
-	DIY n Game. v0.98 Release 6 Crée par Baudoux Loïc, Copyright <? echo date("Y"); ?>
+<div class="col-lg-12 footer" style="padding-top:10px;">
+	<div class="col-lg-12" style="line-height:35px;">
+		<div class="col-lg-6" style="text-align:right;">Rejoignez moi sur Facebook : </div>
+		<div class="col-lg-6" style="text-align:left;"><a href="https://www.facebook.com/diyandgame/"><img class="img-responsive" src="<?= Config::$path_public; ?>/images/icon-facebook.png"></a></div>
+	</div>
+	<div class="col-lg-12" style="line-height:35px;">
+		<div class="col-lg-6" style="text-align:right;">Venez m'aidez à financer les serveurs sur Tipee : </div>
+		<div class="col-lg-6" style="text-align:left;"><a href="https://www.tipeee.com/diy-and-game"><img class="img-responsive" src="<?= Config::$path_public; ?>/images/tipeee_icon.png"></a></div>
+	</div>
+	<p style="text-align:center;" class="col-lg-12">DIY n Game. v0.98 Release 6 Crée par Baudoux Loïc, Copyright <? echo date("Y"); ?></p>
 </div>
